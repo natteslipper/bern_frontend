a6d75c6cc34ea21915866c02e43a21bfd72a3fcb
Bern de Klerk <93846852+natteslipper@users.noreply.github.com>
2021-12-08 08:51:21 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,11 @@
 // JavaScript Document
+
+var hamMenu = document.querySelector("main section:first-of-type");
+
+var hamButton = document.querySelector(".menuKnop");
+
+hamButton.addEventListener("click", toggleHamburgermenu);
+
+function toggleHamburgermenu() {
+    hamMenu.classList.toggle("menuOverlay");
+}
