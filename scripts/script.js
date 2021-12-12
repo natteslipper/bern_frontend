d5123fa3203f24c03c0789b9bd8929bfa03e4bb9
Bern de Klerk <93846852+natteslipper@users.noreply.github.com>
2021-12-12 13:24:33 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -9,3 +9,24 @@ hamButton.addEventListener("click", toggleHamburgermenu);
 function toggleHamburgermenu() {
     document.body.classList.toggle("menuOverlay");
 }
+
+// maak textvlak groter in de gebarentaal pagina
+
+var zichtbaarheid = document.querySelector("main section:nth-of-type(3) article");
+
+var textBlok = document.querySelector("main section:nth-of-type(3)");
+
+var uitklapKnop = document.querySelector("uitklapInfo");
+
+textBlok.addEventListener("click", toggleDropdown);
+
+textBlok.addEventListener("click", togglekleur);
+
+function toggleDropdown() {
+    textBlok.classList.toggle("infoGebaren");
+}
+
+function togglekleur() {
+    zichtbaarheid.classList.toggle("tekstZichtbaar");
+}
+
